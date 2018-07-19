4cd1a5a8153a072c4801685714ed648763a2a641
finominfo <kalman.kovacs@gmail.com>
2018-07-19 15:59:49 +0200

    testing

--- src/main/java/hu/gds/roadsection/RoadResolver.java
+++ src/main/java/hu/gds/roadsection/RoadResolver.java
@@ -3,6 +3,7 @@ package hu.gds.roadsection;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.*;
@@ -77,20 +78,32 @@ public class RoadResolver {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         RoadResolver roadResolver = new RoadResolver();
         Random random = new Random();
         int bigDistance = 0;
         int smallDistance = 0;
-        for (int i = 0; i < 1_000_000; i++) {
-            Data data = roadResolver.get(random.nextDouble() * 1D + 46D, random.nextDouble() * 1D + 16D);
-            if (data.getDistance() > 1_000_000) {
-                bigDistance++;
-            } else if (data.getDistance() < 100) {
-                smallDistance++;
+        int noDistance = 0;
+
+        BufferedReader br = new BufferedReader(new FileReader(new File(CSV)));
+        br.readLine();
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (random.nextInt(100) < 10) {
+                String[] split = line.split(SPLIT);
+                Data data = roadResolver.get(Double.valueOf(split[2]), Double.valueOf(split[3]));
+                if (data.getDistance() > 10_000) {
+                    bigDistance++;
+                } else if (data.getDistance() > 0 && data.getDistance() < 100) {
+                    smallDistance++;
+                } else if (data.getDistance() == 0) {
+                    noDistance++;
+                }
             }
         }
+        br.close();
         System.out.println("Big distances: " + bigDistance);
         System.out.println("Small distances: " + smallDistance);
+        System.out.println("No distances: " + noDistance);
     }
 }
